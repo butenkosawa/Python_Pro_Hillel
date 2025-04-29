fbdace82018404c4a9dc398cb78f500d4f2debe7
butenkosawa <butenkosawa@gmail.com>
2025-04-29 19:23:22 +0300

    ITER_1.2. POC(Proof of Concept). MVP(Minimum Viable Product)

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -25,21 +25,59 @@ storage = [
 
 # CRUD
 def add_student(student: dict):
-    raise NotImplementedError
+    if len(student) != 2:
+        return None
+
+    if not student.get("name") or not student.get("marks"):
+        return None
+    else:
+        storage.append(student)
+        return student
 
 
 def represent_student():
-    raise NotImplementedError
+    for student in storage:
+        info = (
+            "==========================\n"
+            f"Student: {student['name']}\n"
+            f"Marks: {student['marks']}"
+        )
+
+        print(info)
 
 
 def ask_student_payload():
-    raise NotImplementedError
+    ask_prompt = (
+        "Enter student's payload data using text template: "
+        "John Doe;1,2,3,4,5\n"
+        "where 'John Doe' is a full name and [1,2,3,4,5] are marks.\n"
+        "The data must be separated by ';'\n"
+    )
+
+    def parse(data) -> dict | None:
+        name, row_marks = data.split(';')
+        return {
+            "name": name,
+            "marks": [int(item) for item in row_marks.replace(" ", "").split(",")]
+        }
+
+    user_data: str = input(ask_prompt)
+    return parse(user_data)
 
 
-def handle_command():
-    raise NotImplementedError
+def handle_command(command: str):
+    if command == "show":
+        represent_student()
+    elif command == "add":
+        data = ask_student_payload()
+        if data:
+            student: dict = add_student(data)
+            print(f"Student {student['name']} is added")
+        else:
+            print("The student's data is NOT correct. Please try again.")
 
 
-handle_command("show")
-handle_command("add")
-handle_command("show")
+if __name__ == '__main__':
+    handle_command("show")
+    handle_command("add")
+    handle_command("show")
